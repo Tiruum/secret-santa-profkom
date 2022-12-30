ef85926d8c68cc8e50e5ff8aed956b65656937b9
unknown <selintimur@yandex.ru>
2022-12-30 04:49:03 +0300

    Added express

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var PythonShell = require('python-shell');
-//you can use error handling to see if there are any errors
-PythonShell.run('index.py', options, function (err, results) {
-    console.log(rusults);
-    console.log(err);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    //you can use error handling to see if there are any errors
+    PythonShell.run('index.py', options, function (err, results) {
+        console.log(rusults);
+        console.log(err);
+    })
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
